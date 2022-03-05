15689984c52b17fb3b532c4774f51bc04caf0ddc
Isaac-kiptoo-kilimo <isaac.kilimo@student.moringaschool.com>
2022-03-05 16:54:41 +0300

    feature:Selection of elements in javascript

--- js/constants.js
+++ js/constants.js
@@ -4,3 +4,11 @@ const form = document.getElementById('form');
 const form_day = document.getElementById('day');
 const form_month = document.getElementById('month');
 const form_year = document.getElementById('year');
+
+let gender = ''
+
+const results_holder = document.getElementById('results');
+
+const akan_birthday = document.getElementById('akan-birth-day');
+
+const akan_name = document.getElementById('akan-name');
