fb72849acf76a9dc7222df896acf2ffafb767a85
Isaac-kiptoo-kilimo <isaac.kilimo@student.moringaschool.com>
2022-03-05 07:33:24 +0300

    feature:js-This will add select of id's from the html

---
+++ js/constants.js
@@ -0,0 +1,6 @@
+const form_holder = document.getElementById('form-holder');
+
+const form = document.getElementById('form');
+const form_day = document.getElementById('day');
+const form_month = document.getElementById('month');
+const form_year = document.getElementById('year');
